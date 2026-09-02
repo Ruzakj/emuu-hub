160d9420d13ae1dc7b870ab1a18454ffd1cf3616
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-02 23:25:31 +0700

    Restore lifecycle dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.lifecycle:lifecycle-common:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
